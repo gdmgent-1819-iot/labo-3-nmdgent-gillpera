29c1bc663dfadfd2f25bd6d0014e659869453d65
nmdgent-gillpera <gillian.peraltagutierrez@student.arteveldehs.be>
2020-03-11 11:47:08 +0100

    Upload labo3 basis
    #upload

---
+++ config/firebase.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDT6Heal6zcNUPX2szQQOHYnj0vCsaMtbo",
+    authDomain: "iotlabo3-9f72f.firebaseapp.com",
+    databaseURL: "https://iotlabo3-9f72f.firebaseio.com",
+    projectId: "iotlabo3-9f72f",
+    storageBucket: "iotlabo3-9f72f.appspot.com",
+    messagingSenderId: "989428673265",
+    appId: "1:989428673265:web:3063eaae4be2eb1136d750"
+  };
